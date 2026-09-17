5134f8e10f0c14966f7cf65846f6b9ea06a6a8c9
Jayant Yadav <jy8316600@gmail.com>
2026-09-17 19:34:23 +0530

    Time: 9 ms (59.95%), Space: 46.3 MB (38.34%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,9 +1,9 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int n = s.length();
-
-        int r = 0;
         int l = 0;
+        int r = 0;
+
+        int n = s.length();
 
         int maxLen = 0;
         int maxF = 0;
@@ -15,18 +15,20 @@ class Solution {
 
             maxF = Math.max(maxF, hash[s.charAt(r) - 'A']);
 
-            while(((r-l+1) - maxF) > k){
-                hash[s.charAt(l) - 'A']--;
+            while((r-l+1)-maxF > k){
+                maxF = 0;
+                hash[s.charAt(l)-'A']--;
+                
+
                 for(int i = 0 ; i<26 ; i++){
                     maxF = Math.max(maxF, hash[i]);
                 }
-
-                l = l+1;
+                l++;
             }
 
-            if(((r-l+1) - maxF) <= k){
+            
                 maxLen = Math.max(maxLen, r-l+1);
-            }
+            
 
             r++;
         }
